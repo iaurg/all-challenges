62fd4e8314579f6b2903ebcfeabb953d381700fe
Italox <italoaurelior@gmail.com>
2020-01-09 15:16:25 -0300

    test: add test to Event using localstorage mock

--- adisson-global-1/src/__tests__/components/Event.test.js
+++ adisson-global-1/src/__tests__/components/Event.test.js
@@ -42,23 +42,69 @@ const marketsModel = [
   },
 ];
 
-afterEach(cleanup);
+describe('Test Event Component', () => {
+  beforeEach(() => {
+    localStorage.clear();
+  });
 
-it('should render correct event', () => {
-  const { asFragment } = render(
-    <Event id="EVT_1" name="Real Madrid vs Barcelona" markets={marketsModel} />
-  );
-  expect(asFragment()).toMatchSnapshot();
-});
+  it('should render correct event', () => {
+    const { asFragment } = render(
+      <Event
+        id="EVT_1"
+        name="Real Madrid vs Barcelona"
+        markets={marketsModel}
+      />
+    );
+    expect(asFragment()).toMatchSnapshot();
+  });
 
-it('change button color when click', () => {
-  const { getByText } = render(
-    <Event id="EVT_1" name="Real Madrid vs Barcelona" markets={marketsModel} />
-  );
-  const marketButton = getByText('Ronaldo - 1.15');
-  fireEvent.click(marketButton);
-  expect(marketButton).toHaveStyle(`
-    background-image: linear-gradient(rgb(35, 194, 119), rgb(57, 157, 108));
-    color:white;
-  `);
+  it('change button color when click', () => {
+    const { getByText } = render(
+      <Event
+        id="EVT_1"
+        name="Real Madrid vs Barcelona"
+        markets={marketsModel}
+      />
+    );
+    const marketButton = getByText('Real Madrid - 1.23');
+    fireEvent.click(marketButton);
+    expect(marketButton).toHaveStyle(`
+      background-image: linear-gradient(rgb(35, 194, 119), rgb(57, 157, 108));
+      color:white;
+    `);
+  });
+
+  it('should store on localStorage', () => {
+    let { getByText } = render(
+      <Event
+        id="EVT_1"
+        name="Real Madrid vs Barcelona"
+        markets={marketsModel}
+      />
+    );
+    const marketButton = getByText('Real Madrid - 1.23');
+    fireEvent.click(marketButton);
+
+    cleanup();
+
+    ({ getByText } = render(
+      <Event
+        id="EVT_1"
+        name="Real Madrid vs Barcelona"
+        markets={marketsModel}
+      />
+    ));
+
+    expect(localStorage.setItem).toHaveBeenCalledWith(
+      'bets',
+      JSON.stringify({
+        EVT_1: { 'Team to Win': { 'Real Madrid': { betValue: 1.23 } } },
+      })
+    );
+
+    expect(marketButton).toHaveStyle(`
+      background-image: linear-gradient(rgb(35, 194, 119), rgb(57, 157, 108));
+      color:white;
+    `);
+  });
 });
